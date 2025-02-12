495d6b7d400a5a72f885e7ef2e2a8a0422184e9b
cqb13 <cqb13.main@gmail.com>
2025-02-12 09:24:48 -0500

    made text inputs in word ladder builder into a component

---
+++ components/games/wordladder/textField.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+type Props = {
+  value: string;
+  placeholder: string;
+  onChangeAction: (event: any) => void;
+  classModifier?: string;
+};
+
+export default function WordLadderTextField({
+  value,
+  placeholder,
+  onChangeAction,
+  classModifier,
+}: Props) {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => onChangeAction(e)}
+      className={`border-b border-b-black focus:outline-none flex-1 bg-secondary-200 pl-1 rounded text-center p-3 placeholder-accent-700 ${
+        classModifier ? classModifier : ""
+      }`}
+    />
+  );
+}
