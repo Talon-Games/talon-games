efbbf9cd5a8be8fbe366ab6c93b0075ec3b13e9b
cqb13 <cqb13.main@gmail.com>
2025-02-13 09:49:42 -0500

    updated fill-rule and clip-rule to fillRule and clipRule

--- app/games/crossword/archive/page.tsx
+++ app/games/crossword/archive/page.tsx
@@ -131,8 +131,8 @@ export default function Archive() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M5.5 1a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zM3 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1H11v8a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V4h-.5a.5.5 0 0 1-.5-.5M5 4h5v8H5z"
                     fill="#000"
                   />
